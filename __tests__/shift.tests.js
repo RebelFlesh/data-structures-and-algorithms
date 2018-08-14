2d9f08a83574d008150fad5f2762c3c718d96ba7
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-14 15:31:37 -0500

    tested for odd values

--- __tests__/shift.tests.js
+++ __tests__/shift.tests.js
@@ -14,3 +14,10 @@ describe('insertShiftArray',()=>{
     expect(shift([1,2,3,4],'x')).toEqual([1,2,'x',3,4]);
     expect(shift([7,8,9,10],[1,2])).toEqual([7,8,[1,2],9,10]);
   });
+
+  it('inserts value to the right of the center number if the array is odd',()=>{
+    expect(shift([1,2,3],true)).toEqual([1,2,true,3]);
+    expect(shift([5,6,8],7)).toEqual([5,6,7,8]);
+    expect(shift([{type:'ice cream'}],{flavor:'chocolate'})).toEqual([{type:'ice cream'},{flavor:'chocolate'}]);
+  });
+});
