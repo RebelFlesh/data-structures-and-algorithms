2e054b27d31dbf8ed53906510e80a045d58ae53b
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-14 15:29:36 -0500

    added even test

--- __tests__/shift.tests.js
+++ __tests__/shift.tests.js
@@ -8,3 +8,9 @@ describe('insertShiftArray',()=>{
     expect(shift([1,2,3])).toBe(null);
     expect(shift(7,[1,2])).toBe(null);
   });
+
+  it('inserts value in the middle of an even lengthed array',()=>{
+    expect(shift([1,2,3,4],5)).toEqual([1,2,5,3,4]);
+    expect(shift([1,2,3,4],'x')).toEqual([1,2,'x',3,4]);
+    expect(shift([7,8,9,10],[1,2])).toEqual([7,8,[1,2],9,10]);
+  });
