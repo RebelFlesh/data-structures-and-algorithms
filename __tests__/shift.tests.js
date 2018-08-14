ad06ebd73a0680698abda7996ce21d8f26c71db7
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-14 15:27:58 -0500

    wrote validation test

--- __tests__/shift.tests.js
+++ __tests__/shift.tests.js
@@ -1,3 +1,10 @@
 'use strict';
 
 const shift = require('../array_shift');
+
+describe('insertShiftArray',()=>{
+  it('takes 2 valid inputs',()=>{
+    expect(shift()).toBe(null)
+    expect(shift([1,2,3])).toBe(null);
+    expect(shift(7,[1,2])).toBe(null);
+  });
