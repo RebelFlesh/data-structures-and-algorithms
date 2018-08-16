732a014e24e2b1f8c338a2e4ef91d656f37d0d67
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-16 00:15:33 -0500

    added test for valid arguments

--- __tests__/array_binary_search.test.js
+++ __tests__/array_binary_search.test.js
@@ -3,5 +3,10 @@
 const binarySearch = require('../array_binary_search');
 
 describe('binarySeach', ()=>{
-  
+  it('checks if arguments are valid',()=>{
+    expect(binarySearch()).toBe(null);
+    expect(binarySearch([])).toBe(null);
+    expect(binarySearch({'color':'red'})).toBe(null);
+    expect(binarySearch([1,2])).toBe(null);
+  });
 });
