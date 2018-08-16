bdeba03be579a29d5e7c76d3e391de8985f8f9e8
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-16 00:16:08 -0500

    added test for odd arrays

--- __tests__/array_binary_search.test.js
+++ __tests__/array_binary_search.test.js
@@ -9,4 +9,10 @@ describe('binarySeach', ()=>{
     expect(binarySearch({'color':'red'})).toBe(null);
     expect(binarySearch([1,2])).toBe(null);
   });
+  it('gives the index of key inside of arr',()=>{
+    expect(binarySearch([1,2,3],2)).toBe(1);
+    expect(binarySearch([1,2,3,4,5],5)).toBe(4);
+    expect(binarySearch([1,2,3,4,5],2)).toBe(1);
+    expect(binarySearch([1,2,3,4,5,6,7],1)).toBe(0);
+  });
 });
