8f709a5f348eb012e19f657e399dc498cee95671
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-16 00:24:04 -0500

    added search function

--- array_binary_search.js
+++ array_binary_search.js
@@ -2,10 +2,27 @@
 
 function binarySearch(arr,key) {
 
+  function search(arr,key,min,max){
+    var midIndex = Math.ceil((min+max)/2);
+    if(key===arr[midIndex]){
+      return midIndex;
+    } 
+    else if(key>arr[midIndex]){
+      min = midIndex + 1; 
+    }
+    else if(key<arr[midIndex]){
+      max = midIndex - 1;
+    }
+
+    return search(arr,key,min,max);
+  }
+
   var isArray = Array.isArray(arr)
   if(!arr || !key || !isArray || arr.length===0){
     return null;
   }
+
+  return search(arr,key,0,arr.length -1);
 }
 
 
