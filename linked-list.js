df44d5e7d30bd1a29701b7c35fbb24df399857db
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-19 17:33:08 -0500

    added insert method if list is empty

--- linked-list.js
+++ linked-list.js
@@ -6,6 +6,19 @@ class List {
     this.length = 0;
   }
 
+  insert(value){
+    if(this.head === null){
+      this.head = new Node(value);
+    }
+  }
+
+  }
+
+class Node {
+  constructor(value){
+    this.v = value;
+    this.pointer = null;
+  }
 }
 
 module.exports = List;
