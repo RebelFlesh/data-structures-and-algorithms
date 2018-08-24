a19f6b87156218c1a54089bcad8d8e790e9688de
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-24 15:02:06 -0500

    basic structuring for stack and queue classes

---
+++ stacks-and-queues/queues.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class Queue {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+}
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
---
+++ stacks-and-queues/stacks.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class stacks{
+  constructor(){
+    this.head = null;
+    this.length = 0;
+  }
+
+}
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
