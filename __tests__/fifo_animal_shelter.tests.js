a1bdd6f575426ca00103add36e1d5df43a9f8712
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-28 14:38:09 -0500

    added enqueue

--- __tests__/fifo_animal_shelter.tests.js
+++ __tests__/fifo_animal_shelter.tests.js
@@ -3,5 +3,55 @@
 import AnimalShelter from '../fifo_animal_shelter';
 
 describe('AnimalShelter', () => {
+  describe('enqueue', () =>{
+    it('returns undefined if no animal was given', () => {
+      let shelterQueue = new AnimalShelter();
+      expect(shelterQueue.enqueue()).toBe(undefined);
+    });
+    it('can add an animal to an empty shelter', () => {
+      let shelterQueue = new AnimalShelter();
+      let dog = {
+        species:'dog',
+        name:'Fido',
+      }
+      shelterQueue.enqueue(dog);
 
+      expect(shelterQueue.head).not.toBe(null);
+      expect(shelterQueue.tail).not.toBe(null);
+      expect(shelterQueue.head.animal.species).toBe('dog');
+      expect(shelterQueue.length).toBe(1);
+      expect(shelterQueue.numOfDogs).toBe(1);
+      expect(shelterQueue.numOfCats).toBe(0);
+    });
+    it('can add an amimal to an existing shelter', () => {
+      let shelterQueue = new AnimalShelter();
+      let dog1 = {
+        species:'dog',
+        name:'Fido',
+      }
+      let dog2 = {
+        species:'dog',
+        name:'lassy',
+      }
+      shelterQueue.enqueue(dog1);
+      shelterQueue.enqueue(dog2);
+
+      expect(shelterQueue.head.animal.species).toBe('dog');
+      expect(shelterQueue.head.next).not.toBe(null);
+      expect(shelterQueue.head.next.animal.species).toBe('dog');
+      expect(shelterQueue.tail.previous).not.toBe(null);
+      expect(shelterQueue.length).toBe(2);
+      expect(shelterQueue.numOfDogs).toBe(2);
+      expect(shelterQueue.numOfCats).toBe(0);
+
+      let cat = {
+        species:'cat',
+        name:'Garfield',
+      }
+      shelterQueue.enqueue(cat);
+
+      expect(shelterQueue.tail.animal.species).toBe('cat');
+      expect(shelterQueue.numOfCats).toBe(1);
+    });
+  });
 });
--- fifo_animal_shelter.js
+++ fifo_animal_shelter.js
@@ -8,12 +8,40 @@ export default class AnimalShelter{
     this.numOfCats = 0;
     this.numOfDogs = 0;
   }
+
+  enqueue(animal){
+    //if no species were given, dont add an "undefined animal"
+    if(!animal){
+      return undefined;
+    }
+    
+    //Creates new animal
+    let newAnimal = new Animal(animal);
+
+    //If shelter is empty, start a list
+    if(!this.head){
+      this.head = newAnimal;
+      this.tail = newAnimal;
+    }
+
+    //Else add animal to the end
+    this.tail.next = newAnimal;
+    newAnimal.previous =this.tail;
+    this.tail = newAnimal;
+     //Changes num of dogs or cats
+     this.length++;
+    if(newAnimal.animal.species === 'dog'){
+      this.numOfDogs++;
+    }
+    if(newAnimal.animal.species === 'cat'){
+      this.numOfCats++;
+    }
+  }
 }
 
 class Animal{
-  constructor(species){
-    this.animal = species;
-    this.id = null;
+  constructor(animalObject){
+    this.animal = animalObject;
     this.next = null;
     this.previous = null;
   }
