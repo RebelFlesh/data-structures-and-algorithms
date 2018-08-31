4b2502b8938b541b40bade90c2d1f6fa76a6cf77
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-31 14:42:54 -0500

    insert passes tests

--- __tests__/binary_tree.tests.js
+++ __tests__/binary_tree.tests.js
@@ -12,5 +12,34 @@ describe('tree', () => {
       expect(tree.root).not.toBe(null);
       expect(tree.root.value).toBe(4);
     });
+    it('adds a node to the left if it is less than the value', () => {
+      let tree = new Tree();
+
+      tree.insert(4);
+      tree.insert(3);
+      tree.insert(1);
+
+      expect(tree.root.value).toBe(4);
+      expect(tree.root.left.value).toBe(3);
+      expect(tree.root.left.left.value).toBe(1);
+    });
+    it('can add a node to the right of the tree', () => {
+      let tree = new Tree();
+
+      tree.insert(4);
+      tree.insert(6);
+      tree.insert(100);
+
+      expect(tree.root.value).toBe(4);
+      expect(tree.root.right.value).toBe(6);
+      expect(tree.root.right.right.value).toBe(100);
+      expect(tree.root.right.right.parent.parent.value).toBe(4);
+    });
+    it('throws an error if given the same number to insert twice', () => {
+      let tree = new Tree();
+      tree.insert(4);
+      tree.insert(2);
+      expect(() => tree.insert(2)).toThrowError("This node exists in the tree already");
+    });
   });
 });
--- binary_tree.js
+++ binary_tree.js
@@ -6,13 +6,47 @@ export default class BinaryTree{
   }
 
   insert(value){
+    let traverse = function(current){
+      console.log("compairing"+ current.value + "to"+ node.value);
+      if(current.value > node.value){
+        if(current.left === null){
+          node.parent = current;
+          current.left = node;
+        }
+        else{
+          current = current.left;
+          traverse(current);
+        }
+      }
+      else if(current.value < node.value){
+        if(current.right === null){
+          node.parent = current;
+          current.right = node;
+        }
+        else{
+          current = current.right;
+          traverse(current);
+        }
+      }
+      else if(current.value === node.value){
+        throw new Error('This node exists in the tree already');
+      }
+    }
 
+    let node = new Node(value);
+
+    if(!this.root){
+      this.root = node;
+      return;
+    }
+
+    let current = this.root;
+    traverse(current);
   }
-
 }
 
 class Node{
-  constructor(){
+  constructor(value){
     this.value = value;
     this.parent = null;
     this.left = null;
